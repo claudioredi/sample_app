d0301b6af2347962ae182c2137b4a63927f7f298
unknown <Claudio@VAIO-CLAUDIO.(none)>
2012-11-11 12:29:08 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '52de2554d683c423c912d5b3ecffe6acb7173692ea0f14ea7aa7281974b560f8f167af7d9ca1e8796bf9c83f50867b4c6b0314191e32071063bc3a57663f7487'
